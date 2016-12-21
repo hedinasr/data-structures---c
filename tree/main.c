16ad900fbfda9d67c6f35eee0c35778bf62e6f95
Hedi Nasr <h.nsr69@gmail.com>
2016-12-21 12:44:31 +0100

    refactor tree/main.c

--- tree/main.c
+++ tree/main.c
@@ -6,22 +6,26 @@
 #define VALEUR_MAX 100000
 #define VALEUR_MIN 0
 
-int main()
+int
+main ()
 {
-  int alea1,alea2,i; Tree a; int trouve,nb_visites;
-  init(&a);
-  srand((unsigned) time(NULL));
-  for(i=0;i<255;i++)
+  int alea1, alea2, i;
+  Tree a;
+  int trouve, nb_visites;
+  init (&a);
+  srand ((unsigned) time (NULL));
+  for (i = 0; i < 255; i++)
     {
-alea1 = (rand()%(VALEUR_MAX - VALEUR_MIN + 1)) + VALEUR_MIN;
-       insert(alea1,&a);
-     }
-  for(i=0;i<1000000;i++)
-    {
-      alea2 = (rand()%(VALEUR_MAX - VALEUR_MIN + 1)) + VALEUR_MIN;
-search(alea2,&a,&trouve,&nb_visites);
- if(nb_visites == 1) printf("trouve\n");
+      alea1 = (rand () % (VALEUR_MAX - VALEUR_MIN + 1)) + VALEUR_MIN;
+      insert (alea1, &a);
     }
-  clear(&a);
+  for (i = 0; i < 1000000; i++)
+    {
+      alea2 = (rand () % (VALEUR_MAX - VALEUR_MIN + 1)) + VALEUR_MIN;
+      search (alea2, &a, &trouve, &nb_visites);
+      if (nb_visites == 1)
+	printf ("trouve\n");
+    }
+  clear (&a);
   return 0;
 }
